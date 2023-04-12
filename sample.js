8f7b02d4de152af5af63acccaf32d513ab5e3045
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-04-12 17:19:11 +0900

    CDN削除

--- sample.js
+++ sample.js
@@ -1,23 +1,20 @@
-'use strict';
+const keys = document.querySelectorAll('.key');
 
-{
-    //キーを押した時に以下を実行
-    window.addEventListener('keydown', function(e) {
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-        // console.log(audio);
-        if(!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-        // console.log(key);
-        key.classList.add('playing');
-    });
+//キーを押した時に以下を実行
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    // console.log(audio);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    // console.log(key);
+    key.classList.add('playing');
+});
 
-    function removeTransition(e) {
-        if(e.propertyName !== 'transform') return;
-        this.classList.remove('playing');
-    }
-
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
 }
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
